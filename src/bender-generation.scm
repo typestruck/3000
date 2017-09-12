519d3b3f831c72f68d467c7cb12dfe844314b6a6
Eduardo Asafe <azafeh@azafeh.com>
2017-09-12 15:23:55 -0300

    source input text

--- src/bender-generation.scm
+++ src/bender-generation.scm
@@ -3,8 +3,8 @@
 (module bender-generation (generate)
     (import chicken scheme)
     (require-extension matchable srfi-1)
-    (require-library extras random-bsd persistent-hash-map section-combinators uni-combinators srfi-13 irregex)
-    (import (prefix extras ext:) (prefix random-bsd rnd:) (prefix persistent-hash-map hm:) (prefix section-combinators sc:) (prefix uni-combinators un:) (prefix srfi-13 str:) (prefix irregex rg:))    
+    (require-library extras random-bsd persistent-hash-map section-combinators uni-combinators srfi-13 irregex utils)
+    (import (prefix extras ext:) (prefix random-bsd rnd:) (prefix persistent-hash-map hm:) (prefix section-combinators sc:) (prefix uni-combinators un:) (prefix srfi-13 str:) (prefix irregex rg:) (prefix irregex ut:))    
     
     ;;Entry point for text generation.                                
     (define (generate what max-chars)        
@@ -30,7 +30,7 @@
     (define (simple-name max-chars)                                    
         (rg:irregex-replace ",([^,]*)$" (str:string-join (make-name '() (cat-symbol-should-happen '(adjectives adjectives nouns) '(100 40 100))max-chars) ", ") "" 1))    
 
-    ;;A "complex name" is a string containing [<adjective>] <noun> [who | that] [<adverb>] <verb> [<adjective>] [<noun>] [<other>].         
+    ;;A "complex name" is a string containing [<adjective>] <noun> [who | that] [<adverb>] <verb> [<adjective>] [<noun>] [<other>].       
     (define (complex-name max-chars)
         (str:string-join (make-name '() (cat-symbol-should-happen (list 'adjectives 'nouns (if (should-happen 70) "who" "that") 'adverbs 'verbs 'adjectives 'plural-nouns 'other) '(20 100 100 20 100 10 30 5)) max-chars) " "))
 
@@ -62,7 +62,7 @@
 
     ;;Descriptions are markov chains.        
     (define (generate-description max-chars)
-        'description)    
+        source-text)    
 
     ;;Point free helpers.
     (define rs sc:right-section)    
@@ -83,4 +83,10 @@
                                 'plural-nouns (group-by-size (ext:read-lines "data/plural-nouns") (hm:persistent-map))
                                 'verbs (group-by-size (ext:read-lines "data/verbs") (hm:persistent-map))
                                 'adverbs (group-by-size (ext:read-lines "data/adverbs") (hm:persistent-map))
-                                'other (group-by-size (ext:read-lines "data/other") (hm:persistent-map)))))))    
+                                'other (group-by-size (ext:read-lines "data/other") (hm:persistent-map)))))                                                                
+     (define source-text 
+        (let group-by-prefix ((words (string-tokenize (ut:read-all "data/sep-alice-inferno")))
+                              (hash-map (hm:persistent-map)))
+            (match words                
+                ((w . o . r . ds) (group-by-prefix (apply cons o r ds) (hm:map-add hash-map (string-append w o) (cons r (hm:map-ref hash-map (string-append w o) '())))))
+                (_ hash-map))))))                           
