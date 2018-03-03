31052edeb7afe54126ebe05a3e5949e4e4c3bb44
Eduardo Asafe <azafeh@azafeh.com>
2018-03-02 22:12:52 -0300

    configuring a rest api

--- src/bender.scm
+++ src/bender.scm
@@ -1,23 +1,20 @@
 ;abandon all types ye who enter here
-(declare (uses bender-generation bender-conversation))
-(require-library nanomsg utf8-srfi-13 bender-generation bender-conversation)
-(import (prefix bender-generation gen:) (prefix bender-conversation con:) (prefix nanomsg nm:) (prefix utf8-srfi-13 str:))
 
-(define (main args)
-    (let ((socket (nm:nn_socket 'rep)))
-        (nm:nn-bind socket "tcp://0.0.0.1:10080")
-        (let loop ()
-            (let* ((message (nm:nn-recv socket))                   
-                   (delimiter (str:string-index message char-set:whitespace))
-                   (type (string->symbol (str:string-downcase (str:string-take message delimiter))))
-                   (rest (str:string-skip message (+ delimiter 1))))
-                (nm:nn-send socket (if (or (eqv? type 'name) (eq? type 'description)) 
-                                       (gen:generate type (string->number rest))
-                                       (con:conversate rest)))
-                (loop)))))                    
- 
+(declare (uses bender-generation bender-conversation))
+(require-library uri-common spiffy intarweb)
+(import uri-common spiffy intarweb)
+(import (prefix bender-generation gen:) (prefix bender-conversation con:))
+
+(define (handle-greeting continue)
+    (let* ((uri (request-uri (current-request))))
+        (if (equal? (uri-path uri) '(/ "greeting"))
+            (send-response status: 'ok body: "<h1>Hello!</h1>")
+            (continue))))
+
+(define (main args)       
+        (vhost-map `(("localhost" . ,handle-greeting)))
+        (start-server))                            
+
 (cond-expand
     (compiling (main (command-line-arguments)))
     (else))
-        
-        
