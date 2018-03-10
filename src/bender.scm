452bfbd74fd2ab8fc762670fc4aa53a65f787ca4
Eduardo Asafe <azafeh@azafeh.com>
2018-03-10 15:25:21 -0300

    rest api for generation

--- src/bender.scm
+++ src/bender.scm
@@ -1,19 +1,25 @@
 ;abandon all types ye who enter here
 
 (declare (uses bender-generation bender-conversation))
-(require-library uri-common spiffy intarweb)
-(import uri-common spiffy intarweb)
+(require-library uri-common spiffy intarweb spiffy-uri-match intarweb utf8-srfi-13 medea)
+(import uri-common spiffy intarweb spiffy-uri-match utf8-srfi-13 medea)
 (import (prefix bender-generation gen:) (prefix bender-conversation con:))
 
-(define (handle-greeting continue)
-    (let* ((uri (request-uri (current-request))))
-        (if (equal? (uri-path uri) '(/ "greeting"))
-            (send-response status: 'ok body: "<h1>Hello!</h1>")
-            (continue))))
+(define my-custom-routes
+    `(((/ "generate")
+       (GET ,(lambda _
+                (let* ((params (uri-query (request-uri (current-request))))
+                       (type (string->symbol (string-downcase (alist-ref 'type params))))
+                       (chars (string->number (string-downcase (alist-ref 'chars params))))
+                       (response (cond ((eq? type 'description) (gen:generate 'description chars))
+                                       ((eq? type 'name) (gen:generate 'name chars)))))
+                      (send-response status: 'ok body: (json->string response) headers: '((content-type #(text/json ((charset . "utf-8"))))))))))))         
 
 (define (main args)       
-        (vhost-map `(("localhost" . ,handle-greeting)))
-        (start-server))                            
+    (vhost-map `((".*" . ,(uri-match/spiffy my-custom-routes))))
+    (parameterize ((server-port 1337)                                  
+                   (default-response-headers '((content-type #(text/json ((charset . "utf-8"))) (accept-charset utf-8)))))     
+        (start-server)))
 
 (cond-expand
     (compiling (main (command-line-arguments)))
